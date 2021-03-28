0bb1ce2a37d1a6dc49cdd40bc82ffcd8dd0cde6e
johnathanPrisCorp <81187162+johnathanPrisCorp@users.noreply.github.com>
2021-03-28 17:47:17 -0500

    Added bootstrap dependencies

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Col, Row, Container } from "react-bootstrap"
+
+const Home = () => {
+    return (
+        <Col>
+            <Row><h1>This is Home</h1></Row>
+        </Col>
+    )
+}
+
+export default Home
