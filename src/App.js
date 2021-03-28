8dd981776ecd5e85022be39d20803a351ce0a1dc
Johnathan Guzman <johnathanguzman17@gmail.com>
2021-03-28 18:30:06 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home"
+import NavigationBar from "./components/layout/NavigationBar"
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <Home/>
     </div>
   );
